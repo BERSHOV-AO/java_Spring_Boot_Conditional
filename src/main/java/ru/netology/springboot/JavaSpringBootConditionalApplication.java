65c4bfcc1754d6573449edada94041588c2575d5
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-10-28 19:43:54 +0300

    Initial commit

---
+++ src/main/java/ru/netology/springboot/JavaSpringBootConditionalApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootConditionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootConditionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/springboot/JavaSpringBootConditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootConditionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
